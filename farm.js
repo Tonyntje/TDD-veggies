000b23b5cb6d94710db683d95da2310c5dd78020
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2022-05-23 23:56:20 +0200

    Third push for part 3. No there yet but making good progress so far. Understanding the implementation and worth noting the development made so far

--- farm.js
+++ farm.js
@@ -1,11 +1,17 @@
 
 // Write code to fit the tests
 // Get Yield from object to pass
-const getYieldForPlant = plant => plant.yield
+// // 1.1 Added factor formula tot the calc (which works for previous versions)
+// // 1.1.1 Added a wind factor to the equation
+const getYieldForPlant = (plant, sunFactor, windFactor) => {
+    const sunFactorFormula = (sunFactor) ? (100 + plant.factor.sun[sunFactor]) / 100 : 1
+    const windFactorFormula = (windFactor) ? (100 + plant.factor.wind[windFactor]) / 100 : 1
+    return plant.yield * sunFactorFormula * windFactorFormula
+}
 // Get numcrops and multiply with yield via function
-const getYieldForCrop = crop => crop.numCrops * getYieldForPlant(crop.crop)
+const getYieldForCrop = (crop, sun, wind) => crop.numCrops * getYieldForPlant(crop.crop, sun, wind)
 // Map crops into a crop and reduce them to combine them for a total yield
-const getTotalYield = crops => crops.crops.map(crop => getYieldForCrop(crop)).reduce((a, b) => a + b)
+const getTotalYield = (crops, sun, wind) => crops.crops.map(e => getYieldForCrop(e, sun, wind)).reduce((a, b) => a + b)
 // Get the costs of a crop
 // According to the assignment:
 // We need to get the total costs by multiplying 1 by the number of crops
--- farm.test.js
+++ farm.test.js
@@ -86,43 +86,74 @@ describe("Get costs, estimate revenue & profits", () => {
 });
 
 
-// describe("Estimate profits with weather conditions", () => {
-//     test("Calculate Profits by the factors", () => {
-//         // Now we got more data about the crops and which factor will influence the profit
-//         const corn = {
-//             name: "corn",
-//             yield: 3,
-//             salesprice: 2.5,
-//             factor: {
-//                 sun: {
-//                 low: -50,
-//                 medium: 0,
-//                 high: 50,
-//                 },
-//             },
-//         };
-//         const pumpkin = {
-//             name: "pumpkin",
-//             yield: 4,
-//             salesprice: 2,
-//             factor: {
-//                 sun: {
-//                 low: -40,
-//                 medium: 0,
-//                 high: 20,
-//                 },
-//             },
-//         };
-//         const environmentFactors = {
-//             sun: "low",
-//         };
-//         const crops = [
-//             { crop: corn, numCrops: 5 },
-//             { crop: pumpkin, numCrops: 2 },
-//         ];
-//         // Test and refactor code to get right calculations
-//         // Yielf per palnt should be changed including factor as an extra parameter
-//         // Corn has yield of 3, minus 50% the yield should be 1.5
-//         expect(getYieldForPlant(corn, environmentFactors.sun)).toBe(1.5)
-//     });
-// });
+describe("Estimate profits with weather conditions", () => {
+    test("Calculate Profits by the factors", () => {
+        // Now we got more data about the crops and which factor will influence the profit
+        const corn = {
+            name: "corn",
+            yield: 3,
+            salesprice: 2.5,
+            factor: {
+                sun: {
+                    low: -50,
+                    medium: 0,
+                    high: 50,
+                },
+                wind: {
+                    low: 0,
+                    medium: -15,
+                    high: -35, 
+                }
+            },
+        };
+        const pumpkin = {
+            name: "pumpkin",
+            yield: 4,
+            salesprice: 2,
+            factor: {
+                sun: {
+                    low: -40,
+                    medium: 0,
+                    high: 20,
+                },
+                wind: {
+                    low: 0,
+                    medium: -20,
+                    high: -40, 
+                }
+            },
+        };
+        const environmentFactors = {
+            sun: "low",
+            wind: "medium"
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+            { crop: pumpkin, numCrops: 2 },
+        ];
+        // Test and refactor code to get right calculations
+        // Yielf per palnt should be changed including factor as an extra parameter
+        // Corn has yield of 3, minus 50% the yield should be 1.5
+        expect(getYieldForPlant(corn, environmentFactors.sun)).toBe(1.5)
+        // Just in case with pumpkins. Yield of 4, minus 40% the yield should be 2.4
+        expect(getYieldForPlant(pumpkin, environmentFactors.sun)).toBe(2.4)
+        // More test to test if wind is going to work as intended
+        // With just the wind it should be 3, minus 15% which is 2.55
+        expect(getYieldForPlant(corn, undefined, environmentFactors.wind)).toBe(2.55)
+        // Don't forget the pumpkins. 4 minus 20% which is 3.2
+        expect(getYieldForPlant(pumpkin, undefined, environmentFactors.wind)).toBe(3.20)
+        // Now let's try it all
+        // Corn: 3 minus 50% = 1.50 minus 15% = 1.275
+        // Pump: 4 minus 40% = 2.40 minus 20% = 1.92
+        expect(getYieldForPlant(corn, environmentFactors.sun, environmentFactors.wind)).toBe(1.275)
+        expect(getYieldForPlant(pumpkin, environmentFactors.sun, environmentFactors.wind)).toBe(1.92)
+
+        // Let's go full with the factor and multiply this with the number of crops
+        // For Corn this would be   1.275 * 5 = 6.375
+        // For Pumpkin this would be 1.92 * 2 = 3.84
+        // Acumulating to 10.215
+        expect(
+            getTotalYield({crops}, environmentFactors.sun, environmentFactors.wind)
+        ).toBe(10.215)    
+    });
+});
