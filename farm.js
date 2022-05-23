7900128ebf946eb9e8c88d5bbe9d67ec175018af
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2022-05-23 22:29:18 +0200

    Commit second part of assignment done

--- farm.js
+++ farm.js
@@ -6,5 +6,28 @@ const getYieldForPlant = plant => plant.yield
 const getYieldForCrop = crop => crop.numCrops * getYieldForPlant(crop.crop)
 // Map crops into a crop and reduce them to combine them for a total yield
 const getTotalYield = crops => crops.crops.map(crop => getYieldForCrop(crop)).reduce((a, b) => a + b)
+// Get the costs of a crop
+// According to the assignment:
+// We need to get the total costs by multiplying 1 by the number of crops
+const getCostsForCrop = crop => crop.numCrops * 1
+// Get the revenue by multiplying the salesprice with the number of crops
+const getRevenueFromCrops = crop => crop.yield * crop.salesprice
+// Get profit by deducting the cost from the revenue
+const getProfitForCrop = crop => {
+    const costs = getCostsForCrop(crop)
+    const revenue = getRevenueFromCrops(crop.crop)
+    return revenue - costs;
+}
+const getTotalProfit = crops => crops
+            .map(e => getProfitForCrop(e))
+            .reduce((a, b) => a + b)
 
-module.exports = {getYieldForPlant, getYieldForCrop, getTotalYield};
+module.exports = {
+    getYieldForPlant, 
+    getYieldForCrop, 
+    getTotalYield, 
+    getCostsForCrop,
+    getRevenueFromCrops,
+    getProfitForCrop,
+    getTotalProfit
+};
--- farm.test.js
+++ farm.test.js
@@ -1,5 +1,5 @@
 
-const { getYieldForPlant, getYieldForCrop, getTotalYield } = require("./farm");
+const { getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop, getRevenueFromCrops, getProfitForCrop, getTotalProfit } = require("./farm");
 
 describe("getYieldForPlant", () => {
     const corn = {
@@ -52,3 +52,77 @@ describe("getTotalYield", () => {
         expect(getTotalYield({ crops })).toBe(0);
     });
 });
+
+describe("Get costs, estimate revenue & profits", () => {
+    test("Calulate costs per crop", () => {
+        // In my opinion i would look at the structure of data to make tests,
+        // Now i must make a function that basically returns the same value as the input
+        const corn = {
+            name: "corn",
+            yield: 3,
+            salesprice: 2.5
+        };
+        const pumpkin = {
+            name: "pumpkin",
+            yield: 4,
+            salesprice: 2
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+            { crop: pumpkin, numCrops: 2 },
+        ];
+        expect(getCostsForCrop(crops[0])).toBe(5);
+        // Now this is correct. 
+        // I extend the current crops with a sale price to calculate the revenue without the cost deducted
+        // Able to loop this, but let's keep it simple
+        expect(getRevenueFromCrops(corn)).toBe(7.5)
+        expect(getRevenueFromCrops(pumpkin)).toBe(8)
+        // As described in the assignment we have to deduct the cost from the revenue
+        expect(getProfitForCrop(crops[0])).toBe(2.5)
+        expect(getProfitForCrop(crops[1])).toBe(6)
+        // Let's get the total profit from all crops within one function with the help of the other functions
+        expect(getTotalProfit(crops)).toBe(8.5)
+    });
+});
+
+
+// describe("Estimate profits with weather conditions", () => {
+//     test("Calculate Profits by the factors", () => {
+//         // Now we got more data about the crops and which factor will influence the profit
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//             salesprice: 2.5,
+//             factor: {
+//                 sun: {
+//                 low: -50,
+//                 medium: 0,
+//                 high: 50,
+//                 },
+//             },
+//         };
+//         const pumpkin = {
+//             name: "pumpkin",
+//             yield: 4,
+//             salesprice: 2,
+//             factor: {
+//                 sun: {
+//                 low: -40,
+//                 medium: 0,
+//                 high: 20,
+//                 },
+//             },
+//         };
+//         const environmentFactors = {
+//             sun: "low",
+//         };
+//         const crops = [
+//             { crop: corn, numCrops: 5 },
+//             { crop: pumpkin, numCrops: 2 },
+//         ];
+//         // Test and refactor code to get right calculations
+//         // Yielf per palnt should be changed including factor as an extra parameter
+//         // Corn has yield of 3, minus 50% the yield should be 1.5
+//         expect(getYieldForPlant(corn, environmentFactors.sun)).toBe(1.5)
+//     });
+// });
