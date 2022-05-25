d35ade73d09b550e13566e9b582c0a40422a3bf6
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2022-05-25 13:34:59 +0200

    Final push with correct testings. I could've made the code structure differently and add the functions inside the test expectations. Personally i find this messy really quickly and prefer to add nasted functions

--- farm.js
+++ farm.js
@@ -9,28 +9,28 @@ const getYieldForPlant = (plant, sunFactor, windFactor) => {
     return plant.yield * sunFactorFormula * windFactorFormula
 }
 // Get numcrops and multiply with yield via function
-const getYieldForCrop = (crop, sun, wind) => crop.numCrops * getYieldForPlant(crop.crop, sun, wind)
+const getYieldForCrop = (crop, s, w) => crop.numCrops * getYieldForPlant(crop.crop, s, w)
 // Map crops into a crop and reduce them to combine them for a total yield
-const getTotalYield = (crops, sun, wind) => crops.crops.map(e => getYieldForCrop(e, sun, wind)).reduce((a, b) => a + b)
+const getTotalYield = (crops, s, w) => crops.crops.map(e => getYieldForCrop(e, s, w)).reduce((a, b) => a + b)
 // Get the costs of a crop
 // According to the assignment:
 // We need to get the total costs by multiplying 1 by the number of crops
 const getCostsForCrop = crop => crop.numCrops * 1
 // Get the revenue by multiplying the salesprice with the number of crops
-const getRevenueFromCrops = (crop, sun, wind) => Array.from(crop)
-                    .map(e => getYieldForPlant(e.crop, sun, wind) * e.salesprice)
-                    .reduce((a, b) => a + b, 0) 
+const getRevenueForCrop = (crop, s, w) => {
+    return crop.salesprice * getYieldForCrop(crop, s, w)
+}
 
 // Get profit by deducting the cost from the revenue of a crop, not all crops
-const getProfitForCrop = (crop, sun, wind) => {
-    
+const getProfitForCrop = (crop, s, w) => {
     const costs = getCostsForCrop(crop) 
-    const singularRevenue = getRevenueFromCrops([crop]) 
-    return singularRevenue
-    // return revenue - costs;
+    const singularRevenue = getRevenueForCrop(crop, s, w) 
+    return singularRevenue - costs;
 }
-const getTotalProfit = crops => crops
-            .map(e => getProfitForCrop(e))
+// map everything in the crops array with the above functionality
+// Eventually combine everything with reduce to get a total
+const getTotalProfit = (crops, s, w) => crops
+            .map(crop => getProfitForCrop(crop, s, w))
             .reduce((a, b) => a + b)
 
 module.exports = {
@@ -38,7 +38,7 @@ module.exports = {
     getYieldForCrop, 
     getTotalYield, 
     getCostsForCrop,
-    getRevenueFromCrops,
+    getRevenueForCrop,
     getProfitForCrop,
     getTotalProfit
 };
--- farm.test.js
+++ farm.test.js
@@ -1,5 +1,5 @@
 
-const { getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop, getRevenueFromCrops, getProfitForCrop, getTotalProfit } = require("./farm");
+const { getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop, getRevenueForCrop, getProfitForCrop, getTotalProfit } = require("./farm");
 
 describe("getYieldForPlant", () => {
     const corn = {
@@ -69,112 +69,126 @@ describe("Get costs, estimate revenue & profits", () => {
             { crop: corn, numCrops: 5, salesprice: 2.5 },
             { crop: pumpkin, numCrops: 2, salesprice: 2 },
         ];
+        // Corn 5 x 3 = 15 * 2.5 = 37.5 (costs 5)
+        // Corn 2 x 4 = 8  * 2   = 16   (costs 2)
         // Get cost which is number of crops * 1
         // 5 for Corn
         expect(getCostsForCrop(crops[0])).toBe(5);
         // Now this is correct. 
         // I extend the current crops with a sale price to calculate the revenue without the cost deducted
-        // Should be 15.5 combined
-        expect(getRevenueFromCrops(crops)).toBe(15.5)
+        // Should be 37.5 for corn
+        expect(getRevenueForCrop(crops[0])).toBe(37.5)
         // As described in the assignment we have to deduct the cost from the revenue
         // So back to singular crop
-        // For Corn: 7.5 (reveneu) - 5 (cost) 
-        expect(getProfitForCrop(crops[0])).toBe(8.5)
+        // For Corn: 37.5 (revenue) - 5 (cost) = 32.5 (profit)
+        // For Corn: 16.0 (revenue) - 2 (cost) = 14.0 (profit)
+        expect(getProfitForCrop(crops[0])).toBe(32.5)
+        expect(getProfitForCrop(crops[1])).toBe(14)
         // Let's get the total profit from all crops within one function with the help of the other functions
-        // expect(getTotalProfit(crops)).toBe(8.5)
+        // Total Profit should be: 14.0 + 32.5 = 46.5
+        expect(getTotalProfit(crops)).toBe(46.5)
     });
 });
 
 
 describe("Estimate profits with weather conditions", () => {
-    test("Calculate Profits by the factors", () => {
-        // Now we got more data about the crops and which factor will influence the profit
-        const corn = {
-            name: "corn",
-            yield: 3,
-            salesprice: 2.5,
-            factor: {
-                sun: {
-                    low: -50,
-                    medium: 0,
-                    high: 50,
-                },
-                wind: {
-                    low: 0,
-                    medium: -15,
-                    high: -35, 
-                }
+    // Now we got more data about the crops and which factor will influence the profit
+    const corn = {
+        name: "corn",
+        yield: 3,
+        factor: {
+            sun: {
+                low: -50,
+                medium: 0,
+                high: 50,
             },
-        };
-        const pumpkin = {
-            name: "pumpkin",
-            yield: 4,
-            salesprice: 2,
-            factor: {
-                sun: {
-                    low: -40,
-                    medium: 0,
-                    high: 20,
-                },
-                wind: {
-                    low: 0,
-                    medium: -20,
-                    high: -40, 
-                }
+            wind: {
+                low: 0,
+                medium: -15,
+                high: -35, 
+            }
+        },
+    };
+    const pumpkin = {
+        name: "pumpkin",
+        yield: 4,
+        factor: {
+            sun: {
+                low: -40,
+                medium: 0,
+                high: 20,
             },
-        };
-        const environmentFactors = {
-            sun: "low",
-            wind: "medium"
-        };
-        const crops = [
-            { crop: corn, numCrops: 5 },
-            { crop: pumpkin, numCrops: 2 },
-        ];
+            wind: {
+                low: 0,
+                medium: -20,
+                high: -40, 
+            }
+        },
+    };
+    const environmentFactors = {
+        sun: "low",
+        wind: "medium"
+    };
+    const crops = [
+        { crop: corn, numCrops: 5, salesprice: 2.5 },
+        { crop: pumpkin, numCrops: 2, salesprice: 2 },
+    ];
+    // To shorten the code a bit
+    const sun = environmentFactors.sun
+    const wind = environmentFactors.wind
+    
+    test("Get Yield with Weather conditions", () => {
         // Test and refactor code to get right calculations
         // Yielf per palnt should be changed including factor as an extra parameter
         // Corn has yield of 3, minus 50% the yield should be 1.5
-        //expect(getYieldForPlant(corn, environmentFactors.sun)).toBe(1.5)
+        expect(getYieldForPlant(corn, sun)).toBe(1.5)
         // Just in case with pumpkins. Yield of 4, minus 40% the yield should be 2.4
-        //expect(getYieldForPlant(pumpkin, environmentFactors.sun)).toBe(2.4)
+        expect(getYieldForPlant(pumpkin, sun)).toBe(2.4)
         // More test to test if wind is going to work as intended
         // With just the wind it should be 3, minus 15% which is 2.55
-        //expect(getYieldForPlant(corn, undefined, environmentFactors.wind)).toBe(2.55)
+        expect(getYieldForPlant(corn, undefined, wind)).toBe(2.55)
         // Don't forget the pumpkins. 4 minus 20% which is 3.2
-        //expect(getYieldForPlant(pumpkin, undefined, environmentFactors.wind)).toBe(3.20)
+        expect(getYieldForPlant(pumpkin, undefined, wind)).toBe(3.20)
         // Now let's try it all
         // Corn: 3 minus 50% = 1.50 minus 15% = 1.275
         // Pump: 4 minus 40% = 2.40 minus 20% = 1.92
-        //expect(getYieldForPlant(corn, environmentFactors.sun, environmentFactors.wind)).toBe(1.275)
-        //expect(getYieldForPlant(pumpkin, environmentFactors.sun, environmentFactors.wind)).toBe(1.92)
-
+        expect(getYieldForPlant(corn, sun, wind)).toBe(1.275)
+        expect(getYieldForPlant(pumpkin, sun, wind)).toBe(1.92)
         // Let's go full with the factor and multiply this with the number of crops
         // For Corn this would be   1.275 * 5 = 6.375
         // For Pumpkin this would be 1.92 * 2 = 3.84
         // Acumulating to 10.215
-        // expect(
-        //     getTotalYield({crops}, environmentFactors.sun, environmentFactors.wind)
-        // ).toBe(10.215)  
+        expect(
+             getTotalYield({crops}, sun, wind)
+        ).toBe(10.215)  
+    });
+    test("Revenue with Weather factors", () => {
         // With succesfull refactor and test we need to get the factored revenue
         // Ill test this with kOrn
-        // Yield with Factors would be: 1.275
-        // 1.275 * 2.5 = 3.1875 would be the revenue
-        // expect(
-        //     getRevenueFromCrops(corn, environmentFactors.sun, environmentFactors.wind)
-        // ).toBe(3.1875)
-        // Yield with Factors would be: 1.92
-        // 1.92 * 2.0 = 3.84 would be the revenue
-        // expect(
-        //     getRevenueFromCrops(pumpkin, environmentFactors.sun, environmentFactors.wind)
-        // ).toBe(3.84)
+        // Total yield: 6.375 * 2.5 (price) = 15.9375
+        expect(
+            getRevenueForCrop(crops[0], sun, wind)
+        ).toBe(15.9375)
+        // Total yield: 3.84 * 2.00 (price) = 7.68
+        expect(
+            getRevenueForCrop(crops[1], sun, wind)
+        ).toBe(7.68)
+    });
+    test("Profits with the new factors", () => {
         // Now to include the profit factors
-        // Revenue from the Corn with the current factors is 3.1875
-        // The cost is 1 per crop so it should easily be 2.1875
-        // Revenue from the Pumkin with the current factors is 3.84
-        // The cost is 1 per crop so it should easily be 2.84
-        // Acumulated this would a profit 2.1875 + 2.84 = 
-        // expect(
-        //     getProfitForCrop({corn}, environmentFactors.sun, environmentFactors.wind)
-        // ).toBe(5.0275)
+        // Revenue from the Corn with the current factors is 15.9375
+        // The cost is 1 per crop so it should easily be 10.9375
+        expect(
+            getProfitForCrop(crops[0], sun, wind)
+        ).toBe(10.9375)
+        // Revenue from the Pumkin with the current factors is 7.68
+        // The cost is 1 per crop so it should easily be 5.68
+        expect(
+            getProfitForCrop(crops[1], sun, wind)
+        ).toBe(5.68)
+        // Acumulated this would a profit 10.9375 + 5.68 = 16.6175
+        expect(
+            getTotalProfit(crops, sun, wind)
+        ).toBe(16.6175)
     });
 });
